ea6a73a75904192fff0e6c789dc00eeb387d3aaa
chatzimanolis <63492071+NikosChatzimanolis@users.noreply.github.com>
2026-04-09 14:43:20 +0300

    Delete .claude directory

--- .claude/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
-cd /Users/Nico/Documents/day-one-web
-exec /opt/homebrew/bin/node node_modules/.bin/next dev --port 3000
